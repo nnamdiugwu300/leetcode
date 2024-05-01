c404aabf6c52a99dc78304e6c1326a2576a8bfef
Nnamdi <121845426+savagesquad300@users.noreply.github.com>
2024-05-01 14:00:18 -0400

    Update array2.py

--- array2.py
+++ array2.py
@@ -27,3 +27,64 @@ Explanation: "leeto" did not occur in "leetcode", so we return -1.
         return -1  # Needle not found in haystack
 
   ---------------------------------------------------
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+ 
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+
+Solution:
+from typing import List
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+
+        prefix = strs[0]
+        for string in strs[1:]:
+            while string[:len(prefix)] != prefix:
+                prefix = prefix[:len(prefix)-1]
+                if not prefix:
+                    return ""
+
+        return prefixGiven a string s consisting of words and spaces, return the length of the last word in the string.
+
+        ---------------------------------------------------
+  Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal 
+substring
+ consisting of non-space characters only.
+
+ 
+
+Example 1:
+
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+Example 2:
+
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.
+   class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        if not s:
+            return 0
+
+        length = 0
+        last_word = s.strip().split(" ")[-1]
+        length = len(last_word)
+        return length
