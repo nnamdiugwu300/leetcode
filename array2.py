66fa97921c0651d6b5f2af437c38233ca67dc300
Nnamdi <121845426+savagesquad300@users.noreply.github.com>
2024-05-04 13:11:01 -0400

    Update array2.py

--- array2.py
+++ array2.py
@@ -223,3 +223,33 @@ class Solution:
     
         return max_profit
    ---------------------------------------------------------
+   You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+Return true if you can reach the last index, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+Example 2:
+
+Input: nums = [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+Solution:
+from typing import List
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        max_reachable = 0
+        length = len(nums)
+        for i in range(length):
+            if i > max_reachable:
+                return False
+            max_reachable = max(max_reachable, i + nums[i])
+            if max_reachable >= length - 1:
+                return True
+        return True
