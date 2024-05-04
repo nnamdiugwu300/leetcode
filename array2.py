d8ed33534cc5ef32101368446f0dae2ecd627336
Nnamdi <121845426+savagesquad300@users.noreply.github.com>
2024-05-04 18:21:05 -0400

    Update array2.py

--- array2.py
+++ array2.py
@@ -253,3 +253,38 @@ class Solution:
             if max_reachable >= length - 1:
                 return True
         return True
+------------------------------------------------
+You are given a 0-indexed array of integers nums of length n. You are initially positioned at nums[0].
+
+Each element nums[i] represents the maximum length of a forward jump from index i. In other words, if you are at nums[i], you can jump to any nums[i + j] where:
+
+0 <= j <= nums[i] and
+i + j < n
+Return the minimum number of jumps to reach nums[n - 1]. The test cases are generated such that you can reach nums[n - 1].
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: 2
+Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
+Example 2:
+
+Input: nums = [2,3,0,1,4]
+Output: 2
+Solution:
+from typing import List
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        max_reachable = 0
+        last_jump_index = 0
+        jumps = 0
+
+        for i in range(len(nums) - 1):
+            max_reachable = max(max_reachable, i + nums[i])
+            if i == last_jump_index:
+                last_jump_index = max_reachable
+                jumps += 1
+        return jumps
