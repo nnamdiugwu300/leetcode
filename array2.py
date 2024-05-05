b3c031c28d6683cafd7cb4a766a9394300d682ef
Nnamdi <121845426+savagesquad300@users.noreply.github.com>
2024-05-04 23:10:05 -0400

    Update array2.py

--- array2.py
+++ array2.py
@@ -318,3 +318,41 @@ class Solution:
             else:
                 break
         return h_index
+-------------------------------------------------------
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+Example 2:
+
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+ Solution:
+ from typing import List
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        length = len(nums)
+        result = [1] * length
+        
+        # Calculate products to the left and store them in the result array
+        left_product = 1
+        for i in range(length):
+            result[i] *= left_product
+            left_product *= nums[i]
+        
+        # Calculate products to the right and update the result array
+        right_product = 1
+        for i in range(length - 1, -1, -1):
+            result[i] *= right_product
+            right_product *= nums[i]
+        
+        return result
